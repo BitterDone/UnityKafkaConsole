d1a3468be692bdcd1335ef58a54bc241e3c3ae57
Hololens Dev Pc <bitter.daniel@gmail.com>
2019-11-03 13:43:58 -0600

    hangs every loop until a msg exists to consume. Can't click Play/Stop btn in Unity, have to force quit

--- UnityKafkaConsole/Assets/main.cs
+++ UnityKafkaConsole/Assets/main.cs
@@ -76,7 +76,35 @@ public class main : MonoBehaviour
 	void Update()
 	{
 		print("in update, logging:" + logging);
+		if (logging)
+		{
+			print("logging");
+			try
+			{
+				print("try");
+				var cr = c.Consume(cts.Token);
+				print($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+			}
+			catch (ConsumeException e)
+			{
+				print("catch 1");
+				print($"Error occured: {e.Error.Reason}");
+			}
+			catch (OperationCanceledException)
+			{
+				print("catch 2");
+				print("cancelled");
+				// Ensure the consumer leaves the group cleanly and final offsets are committed.
+				c.Close();
+			}
 
+
+			print("finally");
+		}
+		else
+		{
+			print("not logging");
+		}
 	}
 
 	void print(string msg)
